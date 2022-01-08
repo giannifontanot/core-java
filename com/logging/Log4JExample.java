b9cb6be00fd408894a886b6b88af9c9f61be9f67
Gianni Fontanot <giannifontanot@gmail.com>
2022-01-08 09:38:17 -0600

    First commit

---
+++ com/logging/Log4JExample.java
@@ -0,0 +1,13 @@
+package com.logging;
+
+import java.util.logging.Logger;
+public class Log4JExample {
+
+    public static Logger logger = Logger.getLogger("the name is Log4JExample");
+
+
+    public static void main(String[]args){
+        
+
+    }
+}
