e2bca1551c9df4eb90e5806f7eedf4d65735909a
Gianni Fontanot <giannifontanot@gmail.com>
2022-02-02 18:33:41 -0600

    Added Scanner class

---
+++ com/scannerExample/UserInputTester.java
@@ -0,0 +1,20 @@
+package com.scannerExample;
+
+import java.util.Scanner;
+
+public class UserInputTester {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        try{
+            System.out.println("Write something:");
+            while(scanner.hasNext()){
+                System.out.println(scanner.next());
+            }
+        }catch(Exception e) {
+            System.out.println(e.getLocalizedMessage());
+        }finally{
+            scanner.close();
+        }
+    }
+}
