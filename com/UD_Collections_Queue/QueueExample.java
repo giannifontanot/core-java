36574c41c144570d075f08a9bd441badbacd781d
Gianni Fontanot <giannifontanot@gmail.com>
2022-02-08 21:18:17 -0600

    Queue Example

---
+++ com/UD_Collections_Queue/QueueExample.java
@@ -0,0 +1,19 @@
+package com.UD_Collections_Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+    public static void main(String[] args) {
+        //We create a queue, implemented using a linkedlist
+        Queue<String> myQueue = new LinkedList<>();
+
+        myQueue.add("this");
+        myQueue.add("is");
+        myQueue.add("a");
+        myQueue.add("test");
+        while (!myQueue.isEmpty()) {
+            System.out.println(myQueue.poll());
+        }
+    }
+}
